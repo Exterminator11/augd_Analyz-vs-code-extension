198d3b76e18dfdd2e3e2a27571005c15ae49e78f
Exterminator11 <rachit1031@gmail.com>
2023-11-16 17:00:46 +0530

    This seperates code and text

--- backend/app.py
+++ backend/app.py
@@ -2,34 +2,50 @@ import os
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from langchain import HuggingFaceHub, PromptTemplate, LLMChain
+import re
 
 app = FastAPI()
 
 os.environ["HUGGINGFACE_HUB_TOKEN"] = "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"
-model_id = 'mistralai/Mistral-7B-Instruct-v0.1'
+model_id = "mistralai/Mistral-7B-Instruct-v0.1"
 
 template = """{question}"""
-prompt = PromptTemplate(template=template, input_variables=['question'])
+prompt = PromptTemplate(template=template, input_variables=["question"])
 
-mistral_llm = HuggingFaceHub(huggingfacehub_api_token=os.environ['HUGGINGFACE_HUB_TOKEN'],
-                            repo_id=model_id,
-                            model_kwargs={"max_new_tokens":250,"temperature":0.2})
+mistral_llm = HuggingFaceHub(
+    huggingfacehub_api_token=os.environ["HUGGINGFACE_HUB_TOKEN"],
+    repo_id=model_id,
+    model_kwargs={"max_new_tokens": 500, "temperature": 0.2},
+)
 
 mistral_chain = LLMChain(llm=mistral_llm, prompt=prompt)
+
+
+def get_code_and_text(response):
+    code=re.findall(r'```(.+?)```', response, re.DOTALL)
+    text=re.sub(r'```(.+?)```','', response, flags=re.DOTALL)
+    return ''.join(code),text
+
 
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the Text Generation API"}
 
+
 @app.post("/generate_text")
 async def generate_text(request_data: dict):
-    question = request_data.get('question')
+    question = request_data.get("question")
 
     if not question:
-        raise HTTPException(status_code=400, detail="Question is missing in the request.")
+        raise HTTPException(
+            status_code=400, detail="Question is missing in the request."
+        )
 
-    response = mistral_chain.run(question)
-    return {"generated_text": response}
+    response = mistral_chain.run(f"[INST]{question}[/INST]")
+
+    code,text=get_code_and_text(response)
+    return {"text": text, "code": code}
+
 
 # Configure CORS
 app.add_middleware(
@@ -42,4 +58,5 @@ app.add_middleware(
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
