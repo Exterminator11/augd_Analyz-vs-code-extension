c822ed391f6f78a5f92229d285d52127115859ab
Exterminator11 <rachit1031@gmail.com>
2023-11-17 06:17:25 +0530

    Added context to models

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@ import os
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from langchain import HuggingFaceHub, PromptTemplate, LLMChain
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferWindowMemory
 import re
 
 app = FastAPI()
@@ -9,8 +11,14 @@ app = FastAPI()
 os.environ["HUGGINGFACE_HUB_TOKEN"] = "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"
 model_id = "mistralai/Mistral-7B-Instruct-v0.1"
 
-template = """{question}"""
-prompt = PromptTemplate(template=template, input_variables=["question"])
+# template = """{question}"""
+
+template = """
+Current Conversation :
+{history}
+Human : {question}
+"""
+prompt = PromptTemplate(template=template, input_variables=["history","question"])
 
 mistral_llm = HuggingFaceHub(
     huggingfacehub_api_token=os.environ["HUGGINGFACE_HUB_TOKEN"],
@@ -18,13 +26,18 @@ mistral_llm = HuggingFaceHub(
     model_kwargs={"max_new_tokens": 500, "temperature": 0.2},
 )
 
-mistral_chain = LLMChain(llm=mistral_llm, prompt=prompt)
+# mistral_chain = LLMChain(llm=mistral_llm, prompt=prompt)
+mistral_chain = ConversationChain(
+    llm=mistral_llm, 
+    # prompt=prompt, 
+    memory=ConversationBufferWindowMemory(k=5)
+)
 
 
 def get_code_and_text(response):
-    code=re.findall(r'```(.+?)```', response, re.DOTALL)
-    text=re.sub(r'```(.+?)```','', response, flags=re.DOTALL)
-    return ''.join(code),text
+    code = re.findall(r"```(.+?)```", response, re.DOTALL)
+    text = re.sub(r"```(.+?)```", "", response, flags=re.DOTALL)
+    return "".join(code), text
 
 
 @app.get("/")
@@ -42,8 +55,9 @@ async def generate_text(request_data: dict):
         )
 
     response = mistral_chain.run(f"[INST]{question}[/INST]")
+    # response = mistral_chain(f"[INST]{question}[/INST]")["response"]
 
-    code,text=get_code_and_text(response)
+    code, text = get_code_and_text(response)
     return {"text": text, "code": code}
 
 
